9804c139d0859bfd878c8fa7fde7db452fe26098
Ryoichi Shiohama <ryoichi.shiohama@gmail.com>
2020-03-17 02:15:28 +0900

    #1 スケールの表示

--- pages/index.vue
+++ pages/index.vue
@@ -4,21 +4,21 @@
       <label>
         <span>Key</span>
         <select v-model="key" name="key">
-          <option value="c">C</option>
-          <option value="d">D</option>
-          <option value="e">E</option>
-          <option value="f">F</option>
-          <option value="g">G</option>
-          <option value="a">A</option>
-          <option value="b">B</option>
+          <option value="C">C</option>
+          <option value="D">D</option>
+          <option value="E">E</option>
+          <option value="F">F</option>
+          <option value="G">G</option>
+          <option value="A">A</option>
+          <option value="B">B</option>
         </select>
       </label>
 
       <label>
         <span>Scale</span>
         <select v-model="scale" name="scale">
-          <option value="c">Natural Major</option>
-          <option value="MelodicMajor">Melodic Major</option>
+          <option value="major">(Melodic) Major</option>
+          <option value="MelodicMajor">Hormonic Major</option>
           <option value="d">Natural Minor</option>
           <option value="e">Hormonic Minor</option>
           <option value="f">Melodic Minor</option>
@@ -39,128 +39,86 @@ export default {
   components: {},
   data() {
     return {
-      interval: [{ c: 1 }],
-      scaleInterval: {
-        MelodicMajor: [
-          { I: 0 },
-          { II: 2 },
-          { III: 4 },
-          { IV: 5 },
-          { V: 7 },
-          { VI: 9 },
-          { VII: 11 }
-        ]
-        // "MelodicMajor": [
-        //   { I: 0 },
-        //   { II: 2 },
-        //   { III: 4 },
-        //   { IV: 5 },
-        //   { V: 7 },
-        //   { VI: 9 },
-        //   { VII: 11 }
-        // ],
-      },
       VF: Object,
-      key: "",
-      scale: "",
-      scaleOrder: [ "c", "d", "e", "f", "g", "a", "b"],
-      scaleNotes: Object
+      key: "C",
+      scale: "major",
+      scaleList: Object,
+      scaleOrder: ["c", "d", "e", "f", "g", "a", "b"]
+      // scaleNotes: {
+      //   type: Array,
+      //   default: () => []
+      // }
     };
+  },
+  created() {
+    this.scaleList = Scale.names();
   },
   mounted() {
     this.VF = Vex.Flow;
-    let VF = this.VF;
-
-    // Create an SVG renderer and attach it to the DIV element named "boo".
-    var div = document.getElementById("score");
-    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-
-    // Size our svg:
-    renderer.resize(500, 500);
-
-    // And get a drawing context:
-    var context = renderer.getContext();
-
-    // Create a stave at position 10, 40 of width 400 on the canvas.
-    var stave = new VF.Stave(10, 40, 400);
-
-    // Add a clef and time signature.
-    stave.addClef("treble");
-
-    // Connect it to the rendering context and draw!
-    stave.setContext(context).draw();
-
-    // TODO: キーを元にスケール作成
-    let scaleNotes = [this.key + "/4"];
-
-    var notes = [
-      // A quarter-note C.
-      new VF.StaveNote({
-        clef: "treble",
-        keys: ["c/4"],
-        duration: "w"
-      }).addAccidental(0, new VF.Accidental("b")),
-      new VF.StaveNote({ clef: "treble", keys: ["d/4"], duration: "w" }),
-      new VF.StaveNote({ clef: "treble", keys: ["e/4"], duration: "w" }),
-      new VF.StaveNote({ clef: "treble", keys: ["f/4"], duration: "w" }),
-      new VF.StaveNote({ clef: "treble", keys: ["g/4"], duration: "w" }),
-      new VF.StaveNote({ clef: "treble", keys: ["a/4"], duration: "w" }),
-      new VF.StaveNote({ clef: "treble", keys: ["b/4"], duration: "w" })
-
-      // A quarter-note D.
-      // new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "q" }),
-
-      // // A quarter-note rest. Note that the key (b/4) specifies the vertical
-      // // position of the rest.
-      // new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "qr" }),
-
-      // // A C-Major chord.
-      // new VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" })
-    ];
-
-    // Create a voice in 4/4 and add above notes
-    var voice = new VF.Voice({ num_beats: 7, beat_value: 1 });
-    voice.addTickables(notes);
-
-    // Format and justify the notes to 400 pixels.
-    var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
-
-    // Render voice
-    voice.draw(context, stave);
   },
   watch: {
     key: function(newValue) {
-      let scale = this.scale;
       let VF = this.VF;
 
-      console.log(scale);
-      console.log(this.scaleInterval[scale]);
+      // Create an SVG renderer and attach it to the DIV element named "boo".
+      var div = document.getElementById("score");
+      var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
-      // TODO: スケールのインターバルに合わせてキーのスケールノートを生成
-      for (let value of this.scaleInterval[scale]) {
-        // TODO: キーをindex1の状態で並び替え
+      // Size our svg:
+      renderer.resize(500, 500);
 
+      // And get a drawing context:
+      var context = renderer.getContext();
 
-        this.scaleNotes[1] = new VF.StaveNote({
-          clef: "treble",
-          keys: ["c#/4"],
-          duration: "w"
-        });
-        console.log(value);
+      // Create a stave at position 10, 40 of width 400 on the canvas.
+      var stave = new VF.Stave(10, 40, 400);
+
+      // Add a clef and time signature.
+      stave.addClef("treble");
+
+      // Connect it to the rendering context and draw!
+      stave.setContext(context).draw();
+
+      let scaleNotesDatas = Scale.get(this.key + "4 " + this.scale).notes;
+      let scaleNotes = new Array();
+      var accidentalMarkRegExp = new RegExp(/#|b/);
+
+      // スケールのインターバルに合わせてキーのスケールノートを生成
+      for (let [index, note] of scaleNotesDatas.entries()) {
+        // 音名を取得
+        let noteName = note.slice(0, -1);
+
+        // オクターブを取得
+        var noteOct = note.slice(-1);
+
+        // 臨時記号を取得
+        var accidentalMark = note.match(accidentalMarkRegExp);
+        if (null !== accidentalMark) {
+          scaleNotes[index] = new VF.StaveNote({
+            clef: "treble",
+            keys: [noteName.toLowerCase() + "/" + noteOct],
+            duration: "w"
+          }).addAccidental(0, new VF.Accidental(accidentalMark[0]));
+        } else {
+          scaleNotes[index] = new VF.StaveNote({
+            clef: "treble",
+            keys: [noteName.toLowerCase() + "/" + noteOct],
+            duration: "w"
+          });
+        }
       }
-      // TODO: キーを元にスケール作成
-      let scaleNotes = [newValue + "/4"];
 
-      var notes = [
-        // A quarter-note C.
-        new VF.StaveNote({ clef: "treble", keys: ["c#/4"], duration: "w" }),
-        new VF.StaveNote({ clef: "treble", keys: ["d/4"], duration: "w" }),
-        new VF.StaveNote({ clef: "treble", keys: ["cb/4"], duration: "w" }),
-        new VF.StaveNote({ clef: "treble", keys: ["c/4"], duration: "w" }),
-        new VF.StaveNote({ clef: "treble", keys: ["c/4"], duration: "w" }),
-        new VF.StaveNote({ clef: "treble", keys: ["c/4"], duration: "w" }),
-        new VF.StaveNote({ clef: "treble", keys: ["c/4"], duration: "w" })
-      ];
+      // Create a voice in 4/4 and add above notes
+      var voice = new VF.Voice({ num_beats: 7, beat_value: 1 });
+      voice.addTickables(scaleNotes);
+
+      // Format and justify the notes to 400 pixels.
+      var formatter = new VF.Formatter()
+        .joinVoices([voice])
+        .format([voice], 400);
+
+      // Render voice
+      voice.draw(context, stave);
     }
   }
 };
