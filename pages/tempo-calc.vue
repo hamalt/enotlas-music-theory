b991bb83a2943dfdc9d4b98bdd0dd28ca239f3b5
Ryoichi Shiohama <ryoichi.shiohama@gmail.com>
2020-04-04 12:40:55 +0900

    tempoページ集成

--- pages/tempo-calc.vue
+++ pages/tempo-calc.vue
@@ -3,6 +3,8 @@
     <b-field label="BPM">
       <b-numberinput v-model="bpm"></b-numberinput>
     </b-field>
+
+    <b-table :data="data" :columns="columns"></b-table>
   </div>
 </template>
 
@@ -11,7 +13,22 @@ import { Note, Interval, Scale, Key, Chord } from "@tonaljs/tonal";
 export default {
   data() {
     return {
-      bpm: 100
+      bpm: 100,
+      data: [
+        { 'tempo': this.bpm, 'quarter_note': '120' },
+      ],
+      columns: [
+        {
+          field: 'tempo',
+          label: 'Tempo',
+          width: '120',
+          numeric: true
+        },
+        {
+          field: 'quarter_note',
+          label: '1/4',
+        },
+      ]
     }
   }
 }
