6631d81de5bda2701c31eac8e79f0782023599fb
Ryoichi Shiohama <ryoichi.shiohama@gmail.com>
2020-04-14 21:27:44 +0900

    テスト

--- components/GoogleForm.vue
+++ components/GoogleForm.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent="submit" class="google-form">
-    <input v-model="form.name" type="text" name="entry.2005620554" placeholder="sample@mail.co.jp" />
+    <input v-model="form.name" type="text" name="entry.2005620554" placeholder="name" />
     <input
       v-model="form.email"
       type="email"
@@ -8,7 +8,7 @@
       placeholder="sample@mail.co.jp"
     />
     <textarea v-model="form.message" name="entry.839337160" placeholder="message." />
-    <input type="submit" value="Send" />
+    <button type="submit" name="button" value="送信" @click="$router.push('/')">send</button>
   </form>
 </template>
 
@@ -19,6 +19,7 @@ export default {
   data () {
     return {
       form: {
+        isSubmitComplete: false,
         name: "",
         email: "",
         message: ""
@@ -30,18 +31,24 @@ export default {
     * フォーム送信
     */
     submit () {
-      let submitParams = new FormData();
+      const submitParams = new FormData();
+      console.log(this.form.name);
+      console.log(this.form.message);
       submitParams.append('entry.2005620554', this.form.name);
       submitParams.append('entry.1045781291', this.form.email);
       submitParams.append('entry.839337160', this.form.message);
-      let CORS_PROXY = 'https://tools.enotlas.net/';
-      let GOOGLE_FORM_ACTION = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScae4nE8LEy8yzgi2M39dC9uGdwl4kJovrlhNCRRwMDF_rpNA/formResponse';
-      this.$axios.post(CORS_PROXY + GOOGLE_FORM_ACTION, submitParams)
-        .then((test) => {
-          console.log(test);
-          console.log("complete");
-          // this.$router.push('thanks')
-        });
+      console.log(submitParams);
+      const CORS_PROXY = 'https://tools.enotlas.net/';
+      const GOOGLE_FORM_ACTION = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScae4nE8LEy8yzgi2M39dC9uGdwl4kJovrlhNCRRwMDF_rpNA/formResponse';
+      this.$axios.$post(CORS_PROXY + GOOGLE_FORM_ACTION, submitParams)
+      .then(response => {
+        console.log(response);
+        console.log("complete");
+        this.isSubmitComplete = true;
+      })
+      .catch(() => {
+        this.isSubmitComplete = true;
+      });
     }
   }
 }
