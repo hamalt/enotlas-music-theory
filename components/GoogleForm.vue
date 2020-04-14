327f8e0f25d56e3b5698ce87c7c2f5453b932d73
Ryoichi Shiohama <ryoichi.shiohama@gmail.com>
2020-04-14 20:48:11 +0900

    フォームテスト用

--- components/GoogleForm.vue
+++ components/GoogleForm.vue
@@ -1,7 +1,13 @@
 <template>
   <form @submit.prevent="submit" class="google-form">
-    <input v-model="form.email" type="email" name="entry.email" placeholder="sample@mail.co.jp" />
-    <textarea v-model="form.message" name="entry.message" placeholder="message." />
+    <input v-model="form.name" type="text" name="entry.2005620554" placeholder="sample@mail.co.jp" />
+    <input
+      v-model="form.email"
+      type="email"
+      name="entry.1045781291"
+      placeholder="sample@mail.co.jp"
+    />
+    <textarea v-model="form.message" name="entry.839337160" placeholder="message." />
     <input type="submit" value="Send" />
   </form>
 </template>
@@ -13,6 +19,7 @@ export default {
   data () {
     return {
       form: {
+        name: "",
         email: "",
         message: ""
       }
@@ -24,13 +31,16 @@ export default {
     */
     submit () {
       let submitParams = new FormData();
-      submitParams.append('entry.email', this.form.email);
-      submitParams.append('entry.message', this.form.message);
+      submitParams.append('entry.2005620554', this.form.name);
+      submitParams.append('entry.1045781291', this.form.email);
+      submitParams.append('entry.839337160', this.form.message);
       let CORS_PROXY = 'https://tools.enotlas.net/';
-      let GOOGLE_FORM_ACTION = 'https://docs.google.com/forms/d/e/1FAIpQLScae4nE8LEy8yzgi2M39dC9uGdwl4kJovrlhNCRRwMDF_rpNA/formResponse';
+      let GOOGLE_FORM_ACTION = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScae4nE8LEy8yzgi2M39dC9uGdwl4kJovrlhNCRRwMDF_rpNA/formResponse';
       this.$axios.post(CORS_PROXY + GOOGLE_FORM_ACTION, submitParams)
-        .then(() => {
-          this.$router.push('thanks')
+        .then((test) => {
+          console.log(test);
+          console.log("complete");
+          // this.$router.push('thanks')
         });
     }
   }
