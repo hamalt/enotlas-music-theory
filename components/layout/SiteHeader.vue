21c24624b22a3234e0d5e45757ba9acb01890bfc
Ryoichi Shiohama <ryoichi.shiohama@gmail.com>
2020-04-15 15:56:56 +0900

    ナビゲーション修正

--- components/layout/SiteHeader.vue
+++ components/layout/SiteHeader.vue
@@ -3,6 +3,11 @@
     <template slot="brand">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">Enotolas Music tools</b-navbar-item>
     </template>
+
+    <template slot="start">
+      <b-navbar-item tag="nuxt-link" :to="{ path: '/' }" class="is-hidden-desktop">Mode List</b-navbar-item>
+      <b-navbar-item :to="{ path: '/tempo-calc' }" class="is-hidden-desktop">Tempo Calculator</b-navbar-item>
+    </template>
   </b-navbar>
 </template>
 
