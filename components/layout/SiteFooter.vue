efd0b298de88c460bad77d1afa535a5c8e33d39e
Ryoichi Shiohama <ryoichi.shiohama@gmail.com>
2020-04-20 13:18:53 +0900

    #31 説明文追加

--- components/layout/SiteFooter.vue
+++ components/layout/SiteFooter.vue
@@ -9,7 +9,32 @@
         </div>
         <div class="column is-8">
           <div class="section">
-            <div class="content has-text-centered">
+            <div class="content has-text-centered-touch">
+              <div class="site-footer__description">
+                <p>
+                  曲の解析などで参考程度に使えたらなと思ったツールを置いてます。
+                  <br />英語だらけなのは、使用してるプログラムの出力結果が英語なので合わせています。
+                  <br />もし需要があるなら日本語対応するかもしれません。
+                  <br />バグや間違いがあったらフォームから指摘いただけると嬉しいです。
+                </p>
+              </div>
+              <div class="site-footer__library">
+                <h4>使ってるライブラリ</h4>
+                <ul>
+                  <li>
+                    <a href="https://www.vexflow.com/" target="_blank">VexFlow</a>
+                  </li>
+                  <li>
+                    <a href="https://github.com/tonaljs/tonal" target="_blank">@tonaljs/tonal</a>
+                  </li>
+                  <li>
+                    <a href="https://ja.nuxtjs.org/" target="_blank">Nuxt.js</a>
+                  </li>
+                  <li>
+                    <a href="https://buefy.org/" target="_blank">Beufy</a>
+                  </li>
+                </ul>
+              </div>
               <p>
                 <strong>Enotolas Music tools</strong> by
                 <a href="https://twitter.com/_enotlas" target="_blank">Enotlas</a>. The website content
@@ -46,5 +71,33 @@ export default {
   padding-left: 1.5rem;
   background-color: #f7f7f7;
   border-top: solid 1px #eee;
+
+  &__description {
+    margin-bottom: 1rem;
+  }
+
+  &__library {
+    display: block;
+    margin-bottom: 1rem;
+
+    dd {
+      margin: 0;
+    }
+
+    ul {
+      display: flex;
+      margin: 0;
+    }
+
+    li {
+      list-style: none;
+      margin-right: 0.4em;
+      margin-bottom: 0.25em;
+
+      + li {
+        margin-top: 0;
+      }
+    }
+  }
 }
 </style>
